42133d8ebf7e1699de7ec800d941ac17b95ff1e6
Crespo <162322646+Crespo443@users.noreply.github.com>
2026-04-05 11:24:16 +0800

    Delete API/sijentik-backend/dist directory

--- API/sijentik-backend/dist/src/report/report.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class ReportService {
-    private prisma;
-    constructor(prisma: PrismaService);
-    generateCSV(user: any): Promise<string>;
-}
