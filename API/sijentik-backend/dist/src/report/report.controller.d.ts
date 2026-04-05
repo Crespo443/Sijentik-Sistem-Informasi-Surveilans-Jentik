1b2c8f7dcf7a863473886ef6fe28b0f5e52fa2a7
crespo <aprilianocrespo27@gmail.com>
2026-04-05 11:24:43 +0800

    build: generate distribution files and update village service logic

--- API/sijentik-backend/dist/src/report/report.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { ReportService } from './report.service';
-import type { Response } from 'express';
-export declare class ReportController {
-    private reportService;
-    constructor(reportService: ReportService);
-    downloadCSV(req: any, res: Response): Promise<Response<any, Record<string, any>>>;
-}
